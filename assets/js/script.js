796aeb1f9dca2ec61f1bafe2b8da573f985c4c94
ShunWei Hu <87918961+diff30140556@users.noreply.github.com>
2023-01-06 23:46:40 -0800

    edited js 01/06

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,54 @@ const contentEl = document.querySelector('.content');
 const welcomeEl = document.querySelector('.welcome');
 // const quizSectionEl = document.querySelector('.quiz-section');
 
+let quizDataBase = [
+    {
+        question: 'which fruits below is red color?',
+        options: {
+            one: 'banana',
+            two: 'apple',
+            three: 'orange',
+            four: 'pear'
+        }
+    },
+    {
+        question: 'please repeat number 3',
+        options: {
+            one: 'one',
+            two: 'two',
+            three: 'three',
+            four: 'four'
+        },
+    },
+    {
+        question: "what's the sound dogs bark",
+        options: {
+            one: 'meow',
+            two: 'heehee',
+            three: 'ahhh',
+            four: 'woof'
+        },
+    }
+]
 
-startBtnEl.addEventListener('click',startQuiz);
+
+startBtnEl.addEventListener('click', startQuiz);
 
 function startQuiz() {
     console.log('start');
     // welcomeEl.style.display = 'none';
+
+    let randomNum = Math.floor(Math.random() * quizDataBase.length);
+
     contentEl.innerHTML = `<div class="quiz-section">
-    <div class="question">
-        <h2>asdasdsdvfvdfbdfbdflkjfgsdkljvldklkfsdlkfjdflksjlksbdfdfgvdfsvgsdvfds</h2>
-    </div>
-    <ol class="options">
-        <li><button class="quiz-options-btn btn">aslkdaslkdsdvgsd</button></li>
-        <li><button class="quiz-options-btn btn">aslkddvgsd</button></li>
-        <li><button class="quiz-options-btn btn">aslkd asdasd asd ada dasd asdasdas</button></li>
-        <li><button class="quiz-options-btn btn">vgsd</button></li>
-    </ol>
-</div>`
+        <div class="question">
+            <h2>`+ quizDataBase[randomNum].question + `</h2>
+        </div>
+        <ol class="options">
+            <li><button class="quiz-options-btn btn">`+ quizDataBase[randomNum].options.one + `</button></li>
+            <li><button class="quiz-options-btn btn">`+ quizDataBase[randomNum].options.two + `</button></li>
+            <li><button class="quiz-options-btn btn">`+ quizDataBase[randomNum].options.three + `</button></li>
+            <li><button class="quiz-options-btn btn">`+ quizDataBase[randomNum].options.four + `</button></li>
+        </ol>
+    </div>`
 }
